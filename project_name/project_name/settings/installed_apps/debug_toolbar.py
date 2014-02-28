97c870fea34204b9c3a7894f9714f675e5573d8b
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2014-02-28 13:51:52 +0100

    Update debug_toolbar.py

--- project_name/project_name/settings/installed_apps/debug_toolbar.py
+++ project_name/project_name/settings/installed_apps/debug_toolbar.py
@@ -1,5 +1,13 @@
 """Settings for the ``debug_toolbar`` app."""
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
-    'SHOW_TOOLBAR_CALLBACK': lambda x: True,
+    'SHOW_TOOLBAR_CALLBACK': 'VAR_PROJECT_NAME.settings.installed_apps.debug_toolbar.show_toolbar',  # NOQA
 }
+
+def show_toolbar(request):
+    return True
+
+
+def hide_toolbar(request):
+    return False
