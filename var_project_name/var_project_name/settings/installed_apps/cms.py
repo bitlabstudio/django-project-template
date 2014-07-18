37a70bf3af6624695b31de93fc38e2dc1da07d22
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2014-07-18 17:04:57 +0200

    Update cms.py

--- var_project_name/var_project_name/settings/installed_apps/cms.py
+++ var_project_name/var_project_name/settings/installed_apps/cms.py
@@ -1,7 +1,10 @@
 """Settings for the ``cms`` app."""
+from django.utils.translation import ugettext_lazy as _
+
+
 CMS_TEMPLATES = (
-    ('cms/cms.html', 'Standard'),
-    ('cms/cms_contact_form.html', 'Contact Form'),
+    ('cms/cms.html', _('Standard')),
+    ('cms/cms_contact_form.html', _('Contact Form')),
 )
 TEXT_SAVE_IMAGE_FUNCTION = \
     'cmsplugin_filer_image.integrations.ckeditor.create_image_plugin'
