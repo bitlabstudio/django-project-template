75f27e7867a7f01cc90ab9b9c984f1b2b4e17ce6
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2015-03-05 17:39:51 +0100

    Update django-cleanup-mailer-messagelog.sh

--- scripts/django-cleanup-mailer-messagelog.sh
+++ scripts/django-cleanup-mailer-messagelog.sh
@@ -2,4 +2,4 @@
 source $HOME/bin/script-settings.sh
 source $HOME/Envs/$VENV_NAME/bin/activate
 
-$HOME/webapps/$DJANGO_APP_NAME/$PROJECTNAME/manage.py cleanup_mailer_messagelog
+$HOME/project/manage.py cleanup_mailer_messagelog
