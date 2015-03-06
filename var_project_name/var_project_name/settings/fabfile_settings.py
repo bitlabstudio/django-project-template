a979b5ef1085fd70f5276ff6baa7edc21fa4d7f2
Martin Brochhaus <mbrochh@gmail.com>
2015-03-06 14:50:31 +0800

    Updated fabfile_settings

--- var_project_name/var_project_name/settings/fabfile_settings.py
+++ var_project_name/var_project_name/settings/fabfile_settings.py
@@ -19,7 +19,7 @@ VENV_NAME = PROJECT_NAME
 
 # You should keep db role and db name identical on your local machine and on
 # your servers
-DB_ROLE = PROJECT_NAME
+DB_ROLE = 'django'
 DB_NAME = PROJECT_NAME
 DB_DUMP_FILENAME = '{0}.dump'.format(PROJECT_NAME)
 MEDIA_DUMP_FILENAME = '{0}_media.tar.gz'.format(PROJECT_NAME)
@@ -33,7 +33,7 @@ COMPILEMESSAGES_ON_DEPLOYMENT = False
 # Add other code snippets you want to be found. Add a file type to the dict and
 # define a regex, which should be processed
 SYNTAX_CHECK = {
-    '*.js': '(console.log\(\)|alert\(\))',
+    '*.js': '(console.log|alert)',
 }
 # Add files or directories to exclude in the syntax check
 SYNTAX_CHECK_EXCLUDES = [
@@ -57,12 +57,12 @@ LOCAL_COVERAGE_PATH = os.path.join(os.path.dirname(__file__), '../../coverage')
 # ============================================================================
 
 # This should be the name of your user that has sudo access on the server
-LOGIN_USER_DEV = '{0}3'.format(PROJECT_NAME)
-LOGIN_USER_STAGE = '{0}2'.format(PROJECT_NAME)
-LOGIN_USER_PROD = PROJECT_NAME
+LOGIN_USER_DEV = None
+LOGIN_USER_STAGE = None
+LOGIN_USER_PROD = 'django'
 HOST_DEV = None
 HOST_STAGE = None
-HOST_PROD = '{0}.webfactional.com'.format(LOGIN_USER_PROD)
+HOST_PROD = 'ENTER_IP_HERE'
 
 RSYNC_EXCLUDES = [
     'local_settings.py',
@@ -74,44 +74,40 @@ RSYNC_EXCLUDES = [
 # You should keep them identical for all tiers (dev, stage, prod).
 def get_fab_setting(setting_name):
     if setting_name == 'SERVER_APACHE_BIN_DIR':
-        return '/home/{0}/webapps/{1}_django/apache2/bin/'.format(
-            env.user, PROJECT_NAME)
+        return None
     if setting_name == 'SERVER_REPO_ROOT':
         return '/home/{0}/src/{1}/'.format(env.user, PROJECT_NAME)
 
     if setting_name == 'SERVER_REPO_PROJECT_ROOT':
-        # This must have no trailing slash because of rsync
-        return '{0}{1}'.format(
+        # must have trailing space because of rsync
+        return '{0}{1}/'.format(
             get_fab_setting('SERVER_REPO_ROOT'), PROJECT_NAME)
 
     if setting_name == 'SERVER_APP_ROOT':
-        return '/home/{0}/webapps/{1}_django/'.format(
+        # must have no trailing space because of rsync
+        return '/home/{0}/project'.format(
             env.user, PROJECT_NAME)
 
     if setting_name == 'SERVER_PROJECT_ROOT':
-        return '{0}{1}/'.format(
-            get_fab_setting('SERVER_APP_ROOT'), PROJECT_NAME)
+        return '/home/{0}/project/'.format(
+            env.user, PROJECT_NAME)
 
     if setting_name == 'SERVER_REQUIREMENTS_PATH':
-        return '{0}/requirements.txt'.format(
+        return '{0}requirements.txt'.format(
             get_fab_setting('SERVER_REPO_PROJECT_ROOT'))
 
     if setting_name == 'SERVER_MEDIA_ROOT':
-        return '/home/{0}/webapps/{1}_media/'.format(
-            env.user, PROJECT_NAME)
+        return '/home/{0}/project_assets/media/'.format(env.user)
 
     if setting_name == 'SERVER_DB_BACKUP_DIR':
-        return '/home/{0}/backups/{1}/postgres/'.format(
-            env.user, PROJECT_NAME)
+        return '/home/{0}/backups/postgres/'.format(env.user)
 
     if setting_name == 'SERVER_MEDIA_BACKUP_DIR':
-        return '/home/{0}/backups/{1}/media/'.format(
-            env.user, PROJECT_NAME)
+        return '/home/{0}/backups/media/'.format(env.user)
 
     if setting_name == 'SERVER_WSGI_FILE':
         return '{0}{1}/wsgi.py'.format(
             get_fab_setting('SERVER_PROJECT_ROOT'), PROJECT_NAME)
 
 
-def FAB_SETTING(x):
-    return get_fab_setting(x)
+FAB_SETTING = lambda x: get_fab_setting(x)
