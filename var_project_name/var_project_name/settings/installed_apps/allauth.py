9849219a53ff47b257a8669d7fb7fbf145751735
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2015-05-18 09:13:38 +0200

    Update allauth.py

--- var_project_name/var_project_name/settings/installed_apps/allauth.py
+++ var_project_name/var_project_name/settings/installed_apps/allauth.py
@@ -3,8 +3,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
-ACCOUNT_USER_DISPLAY = \
-    'var_project_name.settings.installed_apps.allauth.get_username'
+ACCOUNT_USER_DISPLAY = 'var_project_name.settings.installed_apps.allauth.get_email'
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "none"
@@ -16,6 +15,6 @@ ACCOUNT_PASSWORD_MIN_LENGTH = 7
 LOGIN_REDIRECT_URL = '/'
 
 
-def get_username(user):
-    """Standard return of an ``allauth`` username."""
-    return user.email
+def get_email(profile):
+    """Standard return of an ``allauth`` user."""
+    return profile.email
