929a5a43db157d6324e055536b127ffafc1f459b
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2015-07-28 14:41:48 +0200

    Update views_tests.py

--- var_project_name/var_project_name/tests/views_tests.py
+++ var_project_name/var_project_name/tests/views_tests.py
@@ -1,10 +1,12 @@
 """Tests for the views of the ``var_project_name`` project."""
+from django.test import TestCase
+
 from django_libs.tests.views_tests import ViewRequestFactoryTestMixin
 
 from .. import views
 
 
-class AJAXnonAJAXLoginViewTestCase(ViewRequestFactoryTestMixin):
+class AJAXnonAJAXLoginViewTestCase(ViewRequestFactoryTestMixin, TestCase):
     view_class = views.AJAXnonAJAXLoginView
 
     def test_view(self):
